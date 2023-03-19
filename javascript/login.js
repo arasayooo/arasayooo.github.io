73bcdb44f22d9c176f167d146c35421f5eb49615
arasayooo <farraedna56@gmail.com>
2023-03-19 23:25:53 +0800

    .

--- javascript/login.js
+++
@@ -1,9 +0,0 @@
-const labels = document.querySelectorAll('.form-control label')
-
-labels.forEach(label =>{
-    label.innerHTML = label.innerText //get all the text
-    .split('') //split the letter into the array
-    .map((letter,idx)=> `<span style="transition-delay:${idx*40}ms">${letter}</span>`)//use in array to turn into something else idx mean index start with 0
-    .join('')
-    //turn back into string
-})
